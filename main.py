7a54ab9a8d94520a2a120cda165a6ff026fe24ff
Craig Oda <coda@Craigs-MacBook-Air.local>
2013-11-29 15:54:05 -0800

    Added more documentation.  Changed to a different class structure. Improved collision detection.

--- main.py
+++ main.py
@@ -1,164 +1,308 @@
-__author__ = 'craig'
-import map_loader
-import pygame
-import sys
-from map_loader import Loader
+"""
+This is a test program to teach my teenage son to program Android
+games in Python for his mobile phone.
 
-try:
-    import android
-except ImportError:
-    android = None
+Additional information is available at http://pychildren.blogspot.com
 
-class Hero(pygame.sprite.Sprite):
-    def __init__(self, screensize):
-        pygame.sprite.Sprite.__init__(self)
-        self.screenwidth = screensize[0]
-        self.screenheight = screensize[1]
-        boy = pygame.image.load("boy.png")
-        self.image = pygame.transform.scale(boy, (24, 24))
-        self.rect = self.image.get_rect()
-        #self.rect.center = (self.screenwidth / 2,
-        #                    self.screenheight /2)
-        self.rect.center = (self.screenwidth / 2, 50)
-        # self.rect.center = (240, 160)
-        self.speed = 2
+Steps to build a 2D tile map game
+   1) Mapfile
+      a) read json map file from disk
+      b) load into python dictionary
 
-    def update(self, direction):
-        self.mapdirection = "stop"
-        player_boundary = 10
-        if direction == "right":
-            if self.rect.right < self.screenwidth -player_boundary:
-                self.rect.right += self.speed
-            else:
-                self.mapdirection = "right"
-        elif direction == "left":
-            if self.rect.left > player_boundary:
-                self.rect.left -= self.speed
-            else:
-                self.mapdirection = "left"
-        elif direction == "up":
-            if self.rect.top > player_boundary:
-                self.rect.top -= self.speed
-            else:
-                self.mapdirection = "up"
-        elif direction == "down":
-            if self.rect.bottom < self.screenheight - player_boundary:
-                self.rect.bottom += self.speed
-            else:
-                self.mapdirection = "down"
+   2) Tilesets
+      a) extract list of tilesets from map dictionary
+      b) for each tileset, slice a 32x32 pixel square
+      c) load each tile into a list of all tiles from all the tilesets
 
+   3) Display Tiles
+   4) Display Player
+   5) Move Tiles
+   5) Handle collision
+"""
 
+import json, pygame, sys
+import pprint
 
+RUNTEST = True
 
+def load_map(filename):
+    """
+    This takes a file written with Tiled that contains maps in JSON format.
+    Tiled is freely available here:
+    http://www.mapeditor.org/
 
-SCREENSIZE = (480, 320)
-SCREEN = pygame.display.set_mode(SCREENSIZE)
-RED = (255, 0, 0)
-# the offset is the start position of the map and character
-offset = (-270, -1400)
+    returns a dictionary of the map data.
 
-pygame.init()
+    """
+    mapfile = open(filename)
+    jsondata = mapfile.read()
+    mapdict = json.loads(jsondata)
+    return(mapdict)
 
-if android:
-    android.init()
-    android.map_key(android.KEYCODE_BACK, pygame.K_ESCAPE)
+class Tileset():
+    """
+    In the JSON map that is created with the tile map editor, Tiled, "tilesets" is one of the top-level
+    keys.  This contains a list of dictionaries, one element for each tileset.
+    Each tileset is an image, often in PNG format, that contains many pictures of objects such as
+    trees, grass, rocks, fences, and water.  Each tile in the set is usually a square that is 32x32
+    pixels.  The size of each tile could be bigger or larger than 32.
 
-clock = pygame.time.Clock()
+    This class will slice up the tilesheet into individual tiles and then stare each tile into a
+    list.
+    """
+    def __init__(self, mapdict):
+        self.tilesets = mapdict["tilesets"]
 
-loader = Loader("test_map.json", SCREENSIZE)
-#mapheight = loader.mapheight
-#mapwidth = loader.mapwidth
-hero = Hero(SCREENSIZE)
+    def load(self):
+        """
+        Using the key "image" get the name of the file for each tileset.
+        Load the graphic file into the program as a surface.
+        """
+        tileset_images = []
+        for tilesetdata in self.tilesets:
 
-all_layers, collide_rects = loader.load_layers()
-collide_tilesize = collide_rects[0].size
-collision_tile = pygame.Surface(collide_tilesize)
-collision_tile.set_alpha(75)
-collision_tile.fill(RED)
+            tilesetfile = tilesetdata["image"]
+            tileset = pygame.image.load(tilesetfile)
+            tileset_images.append(tileset)
+        return(tileset_images)
 
-layer_rect = all_layers[0].get_rect()
-#layer_rect.center = SCREEN.get_rect().center
-#layer_rect.bottom = mapheight
+    def slice_tiles(self, tileset_images):
+        """
+        Slices up the tileset and returns a list of all tiles in all layers.
+        """
+        tiles = []
+        for tileset_image in tileset_images:
+            for y in range (0, 1024, 32):
+                for x in range (0, 1024, 32):
+                    rect = pygame.Rect(x, y, 32, 32)
+                    tile = tileset_image.subsurface(rect)
+                    tiles.append(tile)
+        return(tiles)
 
-direction = "stop"
-collide = False
-previous_direction = "stop"
-
-
-layer_rect, collide_rects = loader.initialize_map(offset, layer_rect, collide_rects)
-
-
-while True:
-    if android:
-        if android.check_pause():
-            android.wait_for_resume()
+class Layer():
+    def __init__(self, mapdict, tiles):
+        self.layers = mapdict["layers"]
+        self.tiles = tiles
+        self.width = mapdict["width"]
+        self.height = mapdict["height"]
+        self.tilewidth = tiles[0].get_width()
+        self.tileheight = tiles[0].get_height()
+        self.speed = 1
+        self.screensize = (480, 320)
 
 
-    for event in pygame.event.get():
-        if (event.type == pygame.QUIT) or \
-                (event.type == pygame.KEYDOWN
-                 and event.key == pygame.K_ESCAPE):
+    def load(self):
+        self.mapwidth = self.width * self.tilewidth
+        self.mapheight = self.height * self.tileheight
+        combined_layers = pygame.Surface((self.mapwidth, self.mapheight))
+        collision_list = []
+        layer_num = 1
+        for layer in self.layers:
+            full_map_surface = pygame.Surface((self.width * self.tilewidth, self.height * self.tileheight))
+            data = layer["data"]
+            if layer.has_key("properties"):
+                properties = layer["properties"]
+                if properties.has_key("collision"):
+                    collision_key = properties["collision"]
+                    if collision_key == "1":
+                        collision_index = 0
+                        for y in range (0, self.mapheight, 32):
+                            for x in range (0, self.mapwidth, 32):
+                                if data[collision_index] != 0:
+#                                    print('found collision rectangle')
+                                    collision_rect = pygame.Rect(x, y, 32, 32)
+                                    collision_list.append(collision_rect)
+                                collision_index +=1
+            x = 0
+            y = 0
+            for id in data:
+                if id != 0:
+                    gid = id -1
+                    full_map_surface.blit(self.tiles[gid], (x, y))
+                if x < self.width * (self.tilewidth -1):
+                    x += self.tilewidth
+                else:
+                    x = 0
+                    y += self.tileheight
+            #pygame.image.save(full_map_surface, "layer_{}.png".format(layer_num))
+            full_map_surface.set_colorkey((0,0,0))
+            combined_layers.blit(full_map_surface, (0,0))
+            #pygame.image.save(combined_layers, "map_output.png")
+
+            layer_num += 1
+        return(combined_layers, collision_list)
+
+    def calculate_map_boundary(self):
+        """
+        Track the upper left hand corner of the entire map.
+
+        Remember that the map is bigger than the phone screen.  The upper left coordinate will
+        always be in negative numbers since the upper left corner will be off the visible area
+        of the phone screen.
+
+        If the map starts off with the
+        upper left corner in the upper left corner of the screen, then the position is [0,0]
+        If the player starts off 320 (10 tiles) to the right, the position is [-320, 0].
+        If the player is traveling left, the x coordinate pos[0] should never be greater than 0.
+        It always needs to be a negative value.
+        """
+        self.buffer = self.speed + 1
+        self.map_right_boundary = self.screensize[0] - self.mapwidth + self.buffer
+        self.map_left_boundary = 0 - self.buffer
+        self.map_up_boundary = 0 - self.buffer
+        self.map_down_boundary = self.screensize[1] - self.mapheight + self.buffer
+
+    def check_boundary(self, direction, pos):
+        clear_to_move = True
+        if direction == "left" and pos[0] > self.map_left_boundary:
+            clear_to_move = False
+        if direction == "right" and pos[0] < self.map_right_boundary:
+            clear_to_move = False
+        if direction == "up" and pos[1] > self.map_up_boundary:
+            clear_to_move = False
+        if direction == "down" and pos[1] < self.map_down_boundary:
+            clear_to_move = False
+        return(clear_to_move)
+
+    def check_collision(self, direction, collision_list, clear_to_move):
+        """
+        Check if player is about to collide with a rectangle in the
+        list rectangles that have collision enabled.  The list is called
+        collision_list
+        """
+        player_rect = pygame.Rect(24, 24, 0, 0)
+        player_rect.center = (self.screensize[0]/2, self.screensize[1]/2)
+        for rect in collision_list:
+            rect_tmp = rect.copy()
+            if direction =="left":
+                rect_tmp.centerx += (self.buffer * 2)
+            elif direction == "right":
+                rect_tmp.centerx -= (self.buffer *2)
+            elif direction == "up":
+                rect_tmp.centery += (self.buffer * 2)
+            elif direction == "down":
+                rect_tmp.centery -= (self.buffer * 2)
+            if rect_tmp.colliderect(player_rect):
+                clear_to_move = False
+        return(clear_to_move)
+
+    def update_pos(self, pos, direction, collision_list):
+        clear_to_move = self.check_boundary(direction, pos)
+        clear_to_move = self.check_collision(direction, collision_list, clear_to_move)
+
+        if clear_to_move:
+            if direction == "right":
+                pos[0] -= self.speed
+                for rect in collision_list:
+                    rect.centerx -= self.speed
+            elif direction == "left":
+                pos[0] += self.speed
+                for rect in collision_list:
+                    rect.centerx += self.speed
+            elif direction == "down":
+                pos[1] -= self.speed
+                for rect in collision_list:
+                    rect.centery -= self.speed
+            elif direction == "up":
+                pos[1] += self.speed
+                for rect in collision_list:
+                    rect.centery += self.speed
+        return(pos, collision_list)
+
+
+
+
+
+def run_test(mapdict):
+    """
+    Don't use this.  It was created to run some tests.
+    Takes a dictionary of map data, converted from Tiled data.
+    returns a pygame.Surface that is the entire tileset.
+    """
+    mapkeys =mapdict.keys()
+    tilesets = mapdict["tilesets"]
+    tilesetdata = tilesets[0]
+    tilesetfile = tilesetdata["image"]
+    tileset = pygame.image.load(tilesetfile)
+    return(tileset)
+
+class EventHandler():
+    def quit_game(self, event):
+        if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
 
-    mouse_pos = pygame.mouse.get_pos()
-    if not collide:
-        if mouse_pos[0] > 0 and mouse_pos[0] < 100:
-            direction = "left"
-        elif (mouse_pos[0] < SCREENSIZE[0]) and \
-                (mouse_pos[0] > SCREENSIZE[0] -100):
-            direction = "right"
-        elif mouse_pos[1] > 0 and mouse_pos[1] < 100:
-            direction = "up"
-        elif (mouse_pos[1] < SCREENSIZE[1]) and \
-                (mouse_pos[1] > SCREENSIZE[1] - 100):
-            direction = "down"
-        else:
-            direction = "stop"
+    def set_direction(self, event, direction):
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                direction = "right"
+            elif event.key == pygame.K_LEFT:
+                direction = "left"
+            elif event.key == pygame.K_UP:
+                direction = "up"
+            elif event.key == pygame.K_DOWN:
+                direction = "down"
+        return(direction)
+
+def main():
+    pygame.init()
+
+    SCREENSIZE = (480, 320)
+    SCREEN = pygame.display.set_mode(SCREENSIZE)
+    starting_position = [-220,-200]
+
+    pos = starting_position
+    direction = "stop"
 
 
-    if direction != "stop":
-        previous_direction = direction
+    mapdict = load_map("larger_map.json")
+    tilesets = Tileset(mapdict)
 
-    for collidetile in collide_rects:
-        # SCREEN.blit(collision_tile, collidetile)
-        if hero.rect.colliderect(collidetile):
-            direction = "stop"
-            collide = True
+    tileset_images = tilesets.load()
+    tiles = tilesets.slice_tiles(tileset_images)
 
-    if collide:
-        print("collision", previous_direction)
+    layers = Layer(mapdict, tiles)
 
-        bounce = 5
-        if previous_direction == "right":
-            hero.rect.centerx -= bounce
-            direction = "stop"
-            collide = False
-        elif previous_direction == "left":
-            hero.rect.centerx += bounce
-            direction = "stop"
-            collide = False
-        elif previous_direction == "up":
-            hero.rect.centery += bounce
-            direction = "stop"
-            collide = False
-        elif previous_direction == "down":
-            hero.rect.centery -= bounce
-            direction = "stop"
-            collide = False
+    # this controls the speed of the player moving around the screen
+    layers.speed = 3
+
+    # the default screensize is 480, 320
+    layers.screensize = SCREENSIZE
+    combined_layers, collision_list = layers.load()
+    for rect in collision_list:
+        rect.centerx += starting_position[0]
+        rect.centery += starting_position[1]
+    layers.calculate_map_boundary()
+
+    event_handler = EventHandler()
+
+
+    player = pygame.image.load("img/girl.png").convert_alpha()
+    player_rect = player.get_rect(center = SCREEN.get_rect().center)
 
 
 
+    while True:
+        for event in pygame.event.get():
+            event_handler.quit_game(event)
+            direction = event_handler.set_direction(event, direction)
 
-    hero.update(direction)
-    direction = hero.mapdirection
-    loader.check_boundary(layer_rect)
-    direction = loader.handle_boundary(direction)
-    layer_rect, collide_rects = loader.move_map(direction, layer_rect, collide_rects)
-    for layer in all_layers:
-        SCREEN.blit(layer, layer_rect)
+        pos, collision_list = layers.update_pos(pos, direction, collision_list)
+
+        SCREEN.blit(combined_layers, pos)
+
+        for rect in collision_list:
+            tile = pygame.Surface((32, 32))
+            tile.fill((255, 0, 0))
+            tile.set_alpha(70)
+            SCREEN.blit(tile, rect)
+
+        SCREEN.blit(player, player_rect)
+
+        pygame.display.update()
 
 
-    SCREEN.blit(hero.image, hero.rect)
-    clock.tick (50)
-    pygame.display.update()
+
+
+if __name__ == "__main__":
+    main()
